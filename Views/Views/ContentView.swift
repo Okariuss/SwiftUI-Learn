3b8bd5cafb13b71a08d1af2836b82693be90f8d5
Okan <orkunokann@gmail.com>
2024-07-07 12:03:55 +0300

    Frames learned

---
+++ Views/Views/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Views
+//
+//  Created by Okan Orkun on 7.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .foregroundStyle(.purple)
+                .fontWeight(.heavy)
+            .font(.largeTitle)
+            
+            Text("Merhaba Dünya")
+                .font(.title)
+                .padding()
+                .frame(width: 300, height: 100, alignment: .center)
+            
+            Text("Merhaba Dünya")
+                .font(.title)
+                .padding()
+                .frame(minWidth: 100, idealWidth: 200, maxWidth: 250, minHeight: 100, idealHeight: 100, maxHeight: 100,
+                       alignment: .center)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Views/Views/ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsApp.swift
+//  Views
+//
+//  Created by Okan Orkun on 7.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
